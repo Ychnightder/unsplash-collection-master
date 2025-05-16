05d03daef84dd3a4acf0caae51ae265f018d75bb
Ychnightder <pierreychnightder561@gmail.com>
2025-05-16 21:44:54 +0200

    main

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
+import HeaderC from "./components/HeaderC.vue"
 </script>
 
 <template>
-
+  <HeaderC></HeaderC>
 </template>
 
-<style >
+<style lang="scss" >
 
 </style>
---
+++ src/components/HeaderC.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <div class="gg"></div>
+</template>
+
+<style lang="scss" >
+    .gg{
+        width: 100%;
+        height: 63px;
+        background: #000;
+    }
+</style>
