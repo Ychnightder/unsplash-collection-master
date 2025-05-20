cbf0b50b27f7175370b61eb19128cb950e481b69
Ychnightder <pierreychnightder561@gmail.com>
2025-05-20 23:46:12 +0200

    test

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
-const express = require('expres');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 
+
+  app.get('/api/collections', (req, res) => {
+    res.json(  { id: 2, name: 'City' });
+  });
+
+  app.listen(3000, () => console.log('API on http://localhost:3000'));
