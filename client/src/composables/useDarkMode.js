cb32dd8c855bc67c37734fe744144e7e3165c87a
Ychnightder <pierreychnightder561@gmail.com>
2025-05-28 00:11:24 +0200

    debut vue Navbar.vue

--- client/src/composables/useDarkMode.js
+++ client/src/composables/useDarkMode.js
@@ -1,34 +1,36 @@
-import { ref, watchEffect, onMounted } from 'vue'
-const isDark = ref(false);
+import { ref, onMounted } from 'vue'
 
 export function useDarkMode() {
 
-    const toggleDark = () => {
+    const isDark = ref(false)
+
+    const updateDOM = () => {
+        document.documentElement.classList.toggle('dark', isDark.value)
+    }
+
+    const toggleTheme = () => {
         isDark.value = !isDark.value
-        updateClass()
+        updateDOM()
         localStorage.setItem('theme', isDark.value ? 'dark' : 'light')
     }
-
-
-    const updateClass = () => {
-        if (isDark.value) {
-            document.documentElement.classList.add('dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-        }
-    }
-
 
     onMounted(() => {
         const savedTheme = localStorage.getItem('theme')
-        isDark.value = savedTheme === 'dark'
-        updateClass()
+
+        if (savedTheme) {
+            isDark.value = savedTheme === 'dark'
+        } else {
+            const systemDark = window.matchMedia('(prefers-color-scheme: dark)')
+            isDark.value = systemDark.matches
+
+            systemDark.addEventListener('change', e => {
+                isDark.value = e.matches
+                updateDOM()
+            })
+        }
+
+        updateDOM()
     })
 
-    watchEffect(() => {
-        localStorage.setItem('theme', isDark.value ? 'dark' : 'light')
-    })
-
-    return { isDark, toggleDark }
-
+    return { isDark, toggleTheme }
 }
